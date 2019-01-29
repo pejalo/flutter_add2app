a5680f97ffd79b07d91c5a655526a62a2ff06402
Peter LoBue <pejalo@gmail.com>
2019-01-29 15:08:00 -0800

    Removed Main.storyboard from project

--- Add2App/Add2App/AppDelegate.swift
+++ Add2App/Add2App/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let viewController = ViewController()
+        
+        window = UIWindow()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
